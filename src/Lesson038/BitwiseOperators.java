2f05c4f35972a5f1e837a8857a6ced0e65bd218c
Serg <sergo_nau@ukr.net>
2017-06-03 17:59:14 +0300

    first commit

--- src/Lesson038/BitwiseOperators.java
+++ src/Lesson038/BitwiseOperators.java
@@ -1,4 +1,25 @@
 package Lesson038;
 
 public class BitwiseOperators {
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 13;
+        int c = 0;
+
+        c = a & b;
+        System.out.println("a & b = " + c);
+
+        c = a | b;
+        System.out.println("a | b = " + c);
+
+        c = a ^ b;
+        System.out.println("a ^ b = " + c);
+
+        c = a >> 2;
+        System.out.println("a >> 2 = " + c);
+
+        c = a << 2;
+        System.out.println("a << 2 = " + c);
+
+    }
 }
